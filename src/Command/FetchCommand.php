bd3effdd8d4b7ba40a67366fedfa9b36e68db16b
Svyat Vlasenko <vls.svyat@gmail.com>
2023-04-14 12:12:23 +0300

    feat: fruit:fetch email notification complete

--- src/Command/FetchCommand.php
+++ src/Command/FetchCommand.php
@@ -2,12 +2,13 @@
 
 namespace App\Command;
 
-use App\Entity\Fruit;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
 use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
@@ -16,6 +17,7 @@ use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Contracts\HttpClient\Exception\ExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface as MailerTransportExceptionInterface;
 
 #[AsCommand(
     name: 'fruit:fetch',
@@ -27,15 +29,17 @@ class FetchCommand extends Command
     protected HttpClientInterface $client;
     protected SerializerInterface $serializer;
     protected EntityManagerInterface $entityManager;
+    protected MailerInterface $mailer;
 
 
     const DATASOURCE = 'https://fruityvice.com/api/fruit/all';
 
-    public function __construct(HttpClientInterface $client, SerializerInterface $serializer, EntityManagerInterface $entityManager, string $name = null)
+    public function __construct(HttpClientInterface $client, SerializerInterface $serializer, EntityManagerInterface $entityManager, MailerInterface $mailer, string $name = null)
     {
         $this->client = $client;
         $this->serializer = $serializer;
         $this->entityManager = $entityManager;
+        $this->mailer = $mailer;
 
         parent::__construct($name);
     }
@@ -61,8 +65,23 @@ class FetchCommand extends Command
         foreach ($fruits as $fruit) {
             $this->entityManager->persist($fruit);
         }
-        $output->writeln(count($fruits) . " successfully added to DB");
+
         $this->entityManager->flush();
+        $output->writeln(count($fruits) . " successfully added to DB");
+
+        $email = (new Email())
+            ->from('test_task@test.com')
+            ->to('vls.svyat@gmail.com')
+            ->subject('Fruit are fetched')
+            ->text(count($fruits) . " successfully added to DB");
+        try {
+            $this->mailer->send($email);
+            $output->writeln("Email was successfully sent");
+
+        } catch (MailerTransportExceptionInterface $e) {
+            $output->writeln("Email can`t be sent\n". $e->getMessage());
+        }
+
 
         return Command::SUCCESS;
     }
